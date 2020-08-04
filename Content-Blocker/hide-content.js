5b42b7f6e8083bfe2f04cdb15e87bc1fe7dd39e5
AvinashUmmagani <ummagani.avinash@gmail.com>
2020-08-04 15:00:32 +0530

    Updated Functionality

---
+++ Content-Blocker/hide-content.js
@@ -0,0 +1,60 @@
+var kw = [];
+getkw = async () => {
+
+	await chrome.storage.local.get(["variable"], function (data) {
+		console.log(typeof data);
+		if (data.variable) kw = data.variable;
+		tags = "SPANEMBIULOLI";
+		total = 0;
+		console.log(kw);
+		for (var ii = 0; ii < kw.length; ii++) {
+			o = $(`:contains(${kw[ii]}):not(:has(:contains(${kw[ii]})))`);
+			for (var i = 0; i < o.length; i++) {
+				if (!o[i].parentNode || o[i].parentNode.nodeName === "BODY") {
+					continue;
+				}
+				hideSpoiler(o[i]);
+				total++;
+			}
+		}
+
+		if (total >= 10) {
+			aTags = document.querySelectorAll("a");
+			for(var i=0;i<aTags.length;i++){
+				aTags[i].href = "#"
+			}
+			headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+			for (var i = 0; i < headings.length; i++){ 
+				hideNode(headings[i]);
+			}
+		}
+
+		function hideSpoiler(node) {
+			ancestor = node.parentNode;
+			if (ancestor != null) {
+				if (ancestor.parentNode != null && ancestor.tagName != "BODY")
+					ancestor = ancestor.parentNode;
+				imgs = ancestor.getElementsByTagName("img");
+				for (var i = 0; i < imgs.length; i++)
+					imgs[i].style.webkitFilter = "blur(10px)";
+				lists = ancestor.getElementsByTagName("li");
+				for (var i = 0; i < lists.length; i++) hideNode(lists[i]);
+			}
+
+			if (node == null || node.parentNode == null) return;
+			all_child = node.parentNode.children;
+			for (var i = 0; i < all_child; i++) {
+				var type = all_child[i].tagName;
+				if (tags.match(type) != null) hideNode(all_child[i]);
+			}
+			hideNode(node);
+		}
+
+		function hideNode(node) {
+			node.textContent = "[TEXT BLOCKED: KEYWORD DETECTED]";
+			node.style.color = "red";
+		}
+	});
+};
+getkw();
+
---
+++ Content-Blocker/popup.js
@@ -0,0 +1,98 @@
+
+re = (child) => {
+	child.parentNode.remove(child);
+	const request = indexedDB.open("content-blocker", 1);
+	request.onsuccess = (e) => {
+		db = e.target.result;
+		const tx = db.transaction(["cb_keywords"], "readwrite");
+		const objectstore = tx.objectStore("cb_keywords");
+		objectstore.delete(parseInt(child.id));
+		window.location.reload();
+	};
+};
+function setDOMInfo(info) {
+	console.log(info);
+}
+function msgSender(kw) {
+	// ...query for the active tab...
+
+	// ...and send a request for the DOM info...
+	chrome.runtime.sendMessage(
+		{ from: "popup", subject: kw },
+		setDOMInfo
+	);
+}
+
+window.onload = function () {
+	var butn = document.querySelector("#addButton");
+	var input = document.querySelector("input");
+	var keyword = document.querySelector("#keywords-list");
+	
+	let db = null;
+	const createdb = () => {
+		const request = indexedDB.open("content-blocker", 1);
+
+		request.onupgradeneeded = (e) => {
+			db = e.target.result;
+			db.createObjectStore("cb_keywords", {
+				keypath: "keyword",
+				autoIncrement: true,
+			});
+		};
+
+		request.onsuccess = (e) => {
+			db = e.target.result;
+			viewNotes(db);
+		};
+		request.onerror = (e) => {
+			console.log("error" + e.target.errror);
+		};
+	};
+	createdb();
+
+	butn.addEventListener("click", () => {
+		if (input.value) {
+			keyword.innerHTML =
+				"<div class=keyword-element>" +
+				input.value +"<span class='badge  badge-pill badge-danger float-right'>New</span>"+
+				"</div>" +
+				keyword.innerHTML;
+
+			const tx = db.transaction("cb_keywords", "readwrite");
+			const pNotes = tx.objectStore("cb_keywords");
+			var obj = { keyword: " "+input.value+" " };
+			pNotes.add(obj);
+			input.value = "";
+		}
+	});
+
+	function viewNotes(db) {
+		const tx = db.transaction("cb_keywords", "readonly");
+		const pNotes = tx.objectStore("cb_keywords");
+		const request = pNotes.openCursor();
+		var kw = [];
+		request.onsuccess = (e) => {
+			const cursor = e.target.result;
+			if (cursor) {
+				//do something with the cursor
+				keyword.innerHTML =
+					"<div class=keyword-element>" +
+					cursor.value.keyword +
+					'<button class="remove close"' +
+					"id=" +
+					cursor.key +
+					" " +
+					">X</button></div>" +
+					keyword.innerHTML;
+				kw.push(cursor.value.keyword);
+				cursor.continue();
+			} else {
+				const nodelist = document.querySelectorAll(".remove");
+				const arr = [...nodelist];
+				arr.map((item) => item.addEventListener("click", () => re(item)));
+				msgSender(kw);
+
+			}
+		};
+	}
+};
